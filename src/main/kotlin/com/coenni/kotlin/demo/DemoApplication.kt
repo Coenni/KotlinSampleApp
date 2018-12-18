f82e4eaa23c0aa7ea50ce4a49ff6a3f28edcaae0
coenni <nicat.suleymanov@gmail.com>
2018-12-19 01:27:10 +0200

    init app with sample controller

---
+++ src/main/kotlin/com/coenni/kotlin/demo/DemoApplication.kt
@@ -0,0 +1,12 @@
+package com.coenni.kotlin.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoApplication>(*args)
+}
+
---
+++ src/main/kotlin/com/coenni/kotlin/demo/domain/Greeting.kt
@@ -0,0 +1,4 @@
+package com.coenni.kotlin.demo.domain
+
+data class Greeting(val id: Long, val content: String) {
+}
