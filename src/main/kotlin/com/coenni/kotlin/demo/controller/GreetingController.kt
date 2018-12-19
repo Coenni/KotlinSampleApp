5beeb2dce3b6ca528d6f3d8e8d8ad9ce979af5dd
coenni <nicat.suleymanov@gmail.com>
2018-12-19 03:28:25 +0200

    adding html resolver

--- src/main/kotlin/com/coenni/kotlin/demo/controller/GreetingController.kt
+++ src/main/kotlin/com/coenni/kotlin/demo/controller/GreetingController.kt
@@ -2,11 +2,13 @@ package com.coenni.kotlin.demo.controller
 
 import com.coenni.kotlin.demo.domain.Greeting
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.util.concurrent.atomic.AtomicLong
 
 @RestController
+@RequestMapping("/api")
 class GreetingController {
 
     val counter = AtomicLong()
@@ -14,6 +16,8 @@ class GreetingController {
     @GetMapping("/greeting")
     fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String) =
         Greeting(counter.incrementAndGet(), "Hello, $name")
+
+
 
 
 }
